654c732eaad0a9096244e5615cd8fade6f3166b3
laughingclouds <zenithtaiga@gmail.com>
2021-12-05 19:23:47 +0530

    Added code to check if hand is detected. Did some minor adjustments.

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def thumbIncrementCheck(lmList: list[list[int]]) -> int:
     count = 0
     t_x = lmList[4][1]
     p_x = lmList[17][1]
-    if t_x > p_x:   # RIGHT hand
+    if t_x > p_x:  # RIGHT hand
         if lmList[4][1] >= lmList[2][1]:
             count += 1
-    else:           # LEFT hand
+    else:  # LEFT hand
         if lmList[4][1] <= lmList[2][1]:
             count += 1
     return count
@@ -33,7 +33,7 @@ def textOutput(count, cc) -> str:
     else:
         pass
     return text
-        
+
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -44,37 +44,32 @@ def main():
         img = detector.findHands(img)
         lmlist = detector.findPosition(img, draw=False)
 
+        # If a hand is not detected value will be 0
+        # else non-zero (21)
+        hand_exists = len(lmlist)
+
         tipIDs = [4, 8, 12, 16, 20]
         dipIDs = [2, 7, 11, 15, 19]
         count = 0
         cc = 0
-        if len(lmlist) != 0:
+        if hand_exists:
             for i in range(0, 5):
                 if i == 0:
-                    # # for right hand detection of thumb
-                    # if lmlist[tipIDs[i]][1] >= lmlist[dipIDs[i]][1]:
-                    #     count += 1
                     count += thumbIncrementCheck(lmlist)
                 else:
                     if (lmlist[tipIDs[i]][2] < lmlist[dipIDs[i]][2]) and (
-                        tipIDs[i] == 8 or tipIDs[i] == 12
+                        tipIDs[i] in (8, 12)
                     ):
                         count += 1
                         cc += 1
                     elif lmlist[tipIDs[i]][2] < lmlist[dipIDs[i]][2]:
                         count += 1
-        
-        if count == 0:
-            txt = "STONE"
-        elif count == 2 and cc == 2:
-            txt = "SCISSOR"
-        elif count == 5:
-            txt = "PAPER"
         else:
-            txt = "Nothing"
-        # txt = textOutput(count, cc)
+            count = -1
 
-        cv2.putText(img, str(txt), (10, 140), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)        
+        txt = textOutput(count, cc)
+
+        cv2.putText(img, str(txt), (10, 140), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
 
         cv2.imshow("Image", img)
         # close key isn't working for me
